3727fdcfa947732c984e21f6984212554d1612d2
redxzeta <xzetanzero@gmail.com>
2021-12-26 08:12:38 -0800

    todo app

---
+++ src/main/java/com/demon/slayer/SlayerApplication.java
@@ -0,0 +1,13 @@
+package com.demon.slayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SlayerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SlayerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demon/slayer/SlayerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demon.slayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SlayerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
